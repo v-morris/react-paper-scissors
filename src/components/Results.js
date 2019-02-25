a9c02dc31e6bba16dbe7e464d7cac03a67e309b2
v-morris <digidarkstudio@gmail.com>
2019-02-25 00:17:17 -0500

    added player winner conditionals

--- src/components/Results.js
+++ src/components/Results.js
@@ -7,19 +7,41 @@ class Results extends Component {
         this.state =
             {
                 player: null,
-                computer: null
+                computer: null,
+                result: null
             }
+
+        this.playerWinner = this.playerWinner.bind(this);
     }
+
+    playerWinner() {
+        let player = this.props.player;
+        let computer = this.props.computer;
+        return (
+            (player === "paper" && computer === "rock") ||
+            (player === "scissors" && computer === "paper") ||
+            (player === "rock" && computer === "scissors")
+        );
+    }
+
+    // componentDidMount(){
+    //     this.playerWinner() ?
+    //     this.setState({result : "You're a winner!"}) :
+    //     this.setState({result : "You lose!"})
+    //     console.log("--------" + this.state.result)
+    // }
 
     render() {
         let player = this.props.player;
         let computer = this.props.computer;
         
+        console.log("Results Page" + player + " " + computer)
         return (
             <div>
-                <span>{player}</span><br/>
-                <span>{computer}</span>
-                <Score player={player} computer={computer}/>
+                <span>{player}</span><br />
+                <span>{computer}</span><br/>
+                <span>{this.state.result}</span>
+                <Score player={player} computer={computer} />
             </div>
         )
     }
